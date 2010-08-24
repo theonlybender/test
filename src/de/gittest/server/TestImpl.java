095eac1d8b7b816147a3811e9462eaf89fc11ef6
Theonlybender <Theonlybender@t-online.de>
2010-08-25 00:39:38 +0200

    new file

---
+++ src/de/gittest/server/TestImpl.java
@@ -0,0 +1,9 @@
+package de.gittest.server;
+
+import de.gittest.client.GreetingService;
+import de.gittest.shared.FieldVerifier;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+public class TestImpl {
+
+}
